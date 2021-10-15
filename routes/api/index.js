73c19ec86b49a18c2cbc0e4f17fed40ce6aca8fe
Brian <bgswilde@gmail.com>
2021-10-14 23:04:43 -0500

    added comment schema, routes, and also added in populate methods in routes.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const pizzaRoutes = require('./pizza-routes.js');
+const commentRoutes = require('./comment-routes');
 
 router.use('/pizzas', pizzaRoutes);
-
+router.use('/comments', commentRoutes)
 module.exports = router;
